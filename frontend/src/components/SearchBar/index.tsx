3a9efaa8524b778f15511e74eb6fb90f53d79f53
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-09-28 17:23:25 -0300

    feat: search bar tablet

--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -6,11 +6,12 @@ import Image from 'next/image';
 const SearchBar = () => {
     return (
         <section>
-            <div className="flex pt-6 px-4 gap-4">
-                <div className="w-6 h-6">
+            <div className="flex pt-6 px-4 gap-4 md:pt-0.5 md:px-6">
+                <div className="w-6 h-6 md:w-8 md:h-8">
                     <Image src={searchIcon} alt="Search Icon" />
                 </div>
-                <input className="w-full bg-transparent text-white focus:outline-none placeholder:opacity-50 placeholder:font-outfit placeholder:font-light placeholder:text-[16px] focus:caret-redTomato font-outfit font-light text-[16px]" type="text" placeholder="Search for movies or TV series"/>
+                <input className="w-full bg-transparent text-white border border-transparent focus:outline-none placeholder:opacity-50 placeholder:font-outfit
+                placeholder:font-light placeholder:text-[16px] caret-redTomato font-outfit font-light text-[16px] md:placeholder:text-medium" type="text" placeholder="Search for movies or TV series" maxLength={60} />
             </div>
         </section>
     );
